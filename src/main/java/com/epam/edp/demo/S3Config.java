e2d90a20a91c779b31703bd5977a2e6bb4c93a3a
ZhannaGuseva <zhannaguseva2020@gmail.com>
2025-03-18 13:24:31 +0200

    Add sts dependency to pom

--- src/main/java/com/epam/edp/demo/S3Config.java
+++ src/main/java/com/epam/edp/demo/S3Config.java
@@ -1,5 +1,7 @@
 package com.epam.edp.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +12,21 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
+    private static final Logger logger = LoggerFactory.getLogger(S3Config.class);
+
     @Value("${region}")
     private String region;
 
     @Bean
     S3Client s3Client() {
+        WebIdentityTokenFileCredentialsProvider credentialsProvider = WebIdentityTokenFileCredentialsProvider.create();
+
+        logger.info("Using AWS credentials provider: {}", credentialsProvider.getClass().getSimpleName());
+        logger.info("Web Identity Token will be used for authentication.");
+
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
