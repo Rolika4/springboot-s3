8b6fa89a47f310b67ac1e5c1056bdce89f8b7a00
ZhannaGuseva <zhannaguseva2020@gmail.com>
2025-03-18 16:35:51 +0200

    Test logging test2

--- src/main/java/com/epam/edp/demo/S3Config.java
+++ src/main/java/com/epam/edp/demo/S3Config.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -16,17 +16,26 @@ public class S3Config {
 
     @Value("${region}")
     private String region;
+	
+	@Bean
+	DefaultCredentialsProvider credentialsProvider() {
+        return DefaultCredentialsProvider.create();
+    }
 
     @Bean
     S3Client s3Client() {
-        WebIdentityTokenFileCredentialsProvider credentialsProvider = WebIdentityTokenFileCredentialsProvider.create();
-
-        logger.info("Using AWS credentials provider: {}", credentialsProvider.getClass().getSimpleName());
-        logger.info("Web Identity Token will be used for authentication.");
-
+        
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(credentialsProvider)
+                .credentialsProvider(credentialsProvider())
                 .build();
+    }
+	
+	@Bean
+    StsClient stsClient() {
+        return StsClient.builder()
+            .credentialsProvider(credentialsProvider())
+            .region(Region.of(region))
+            .build();
     }
 }
