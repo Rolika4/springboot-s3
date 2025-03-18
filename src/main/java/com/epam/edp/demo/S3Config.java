f87c4daf5186923a6e75e42542286c8607689197
ZhannaGuseva <zhannaguseva2020@gmail.com>
2025-03-18 16:38:49 +0200

    Test logging add sts dep

--- src/main/java/com/epam/edp/demo/S3Config.java
+++ src/main/java/com/epam/edp/demo/S3Config.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.services.sts.StsClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
